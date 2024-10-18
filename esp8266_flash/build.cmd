18ee0ffa5cde104f9ba078a9541fdc80aa75f1de
revantharunachalam <revanth.sbioa@gmail.com>
2024-10-18 23:31:17 +0530

    [https://github.com/revantharunachalam/easy-electronics-esp-relay/issues/1]: build.cmd in for local building process.

--- esp8266_flash/build.cmd
+++ esp8266_flash/build.cmd
@@ -1,27 +1,22 @@
-@REM Initialize configuration file
+@echo off
+
+REM Initialize configuration file and find esp8266
+echo Initialize configuration file and find esp8266
 arduino-cli config init --overwrite --dest-file esp8266_flash-cli.yaml
-
-@REM Creating new sketch (.ini file) - Not needed for CI/CD
-@REM arduino-cli sketch new .
-
-@REM Display the list of boards
-arduino-cli board list --config-file esp8266_flash-cli.yaml
-
-@REM Searching ESP8266
-arduino-cli core search esp8266 --config-file esp8266_flash-cli.yaml
-
-@REM Updating index which has ESP8266 related files (Need to add https://arduino.esp8266.com/stable/package_esp8266com_index.json in config file)
+arduino-cli config add board_manager.additional_urls http://arduino.esp8266.com/stable/package_esp8266com_index.json
 arduino-cli core update-index --config-file esp8266_flash-cli.yaml
+arduino-cli core search esp8266 --config-file esp8266_flash-cli.yaml
+echo ----------------------------------------------
 
-@REM Listing all the boards after updating index
+REM Install platform
+echo Install platform
 arduino-cli board listall --config-file esp8266_flash-cli.yaml
+arduino-cli core install esp8266:esp8266
+echo ----------------------------------------------
 
-@REM Clean build directory
-del /q 1_build
-mkdir 1_build
-
-@REM Compile .ino file
+REM Compile Sketch
+echo Compile Sketch
+rmdir /S /Q "1_build"
+mkdir "1_build"
 arduino-cli compile --fqbn esp8266:esp8266:nodemcu . --config-file esp8266_flash-cli.yaml --output-dir 1_build
-
-@REM Flash ESP8266 binary
-arduino-cli upload --port COM7 --input-dir 1_build --fqbn esp8266:esp8266:nodemcu . --config-file esp8266_flash-cli.yaml
+echo ----------------------------------------------
